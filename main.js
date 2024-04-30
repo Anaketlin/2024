db20e98b508e643f19e7a4db8f5b4c30f4ce617a
Estudante Alura <105929920+Anaketlin@users.noreply.github.com>
2024-04-30 11:25:04 -0300

    .

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++){
-  botoes[i].onclick = function(){
-    for(let j = 0; j < botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
     }
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
-  }
+  };
 }
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-10T23:59:59");
 const tempoObjetivo2 = new Date("2024-12-09T23:59:59");
 const tempoObjetivo3 = new Date("2024-12-08T23:59:59");
 const tempoObjetivo4 = new Date("2024-12-07T23:59:59");
 
-const tempos = {
-  tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+for (let i = 0; i < contadores.length; i++) {
+  contadores[i].textContent = calculaTempo(tempos[i]);
 }
 
-for (let i=0; i< contadores.length; i++){
-  calculaTempo[i].textContent = calculaTempo(tempos[i]);
-}
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-function calculaTempo(tempoObjetivo1){
+function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo1 - tempoAtual;
+  let tempoFinal = tempoObjetivo - tempoAtual;
   let segundos = Math.floor(tempoFinal / 1000);
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
@@ -38,8 +36,8 @@ function calculaTempo(tempoObjetivo1){
   minutos %= 60;
   horas %= 24;
 
-  return dias + "dias"
-   + horas + "horas" 
-   + minutos + "minutos" 
-   + segundos + "segundos";
+  return dias + " dias " +
+         horas + " horas " +
+         minutos + " minutos " +
+         segundos + " segundos";
 }
